d42f370120d5c04a7e2d99f700f7ed02cf493f46
Youri BOUGRE <yobougre@e2r12p6.clusters.42paris.fr>
2022-06-02 14:40:47 +0200

    [UPDATE] first commit

---
+++ includes/includes.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yobougre <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 13:34:51 by yobougre          #+#    #+#             */
+/*   Updated: 2022/06/02 14:26:04 by yobougre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDES_H
+# define INCLUDES_H
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "struct.h"
+
+#endif
